d52218f58391d20e7447b9a788f609eab10a9ed5
DureevaElena <elenadureeva903@gmail.com>
2024-06-18 02:50:53 +0300

    create/update/delete stageNote (этапы)

--- configurations/todo_app/serializers.py
+++ configurations/todo_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Note
+from .models import Note, StageNote
 from django.contrib.auth import get_user_model
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -18,4 +18,20 @@ class NoteSerializer(serializers.ModelSerializer):
 class NoteSaveSerializer(serializers.ModelSerializer):
     class Meta:
         model=Note
+        fields="__all__"
+
+
+
+
+class StageNoteSerializer(serializers.ModelSerializer):
+    authorstage=serializers.SerializerMethodField("get_author")
+    class Meta:
+        model=StageNote
+        fields="__all__"
+    def get_author(self,model:StageNote):
+        return AuthorSerializer(model.authorstage,many=False).data
+
+class StageNoteSaveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=StageNote
         fields="__all__"
--- configurations/todo_app/views.py
+++ configurations/todo_app/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
-from .models import Note
-from .serializers import NoteSaveSerializer,NoteSerializer
-from rest_framework.generics import (CreateAPIView ,
-                                     UpdateAPIView,
-                                     DestroyAPIView,
-                                     ListAPIView,
-                                     RetrieveUpdateDestroyAPIView,
-                                     ListCreateAPIView
-                                     )
+from .models import Note, StageNote
+from .serializers import NoteSaveSerializer,NoteSerializer, StageNoteSaveSerializer, StageNoteSerializer
+from rest_framework.generics import (CreateAPIView , RetrieveUpdateDestroyAPIView, ListCreateAPIView)
 
 @api_view(["POST"])
 def createNote(request):
@@ -24,17 +18,12 @@ def createNote(request):
         return Response(NoteSerializer(note,many=False).data)
     return Response({})
 
+
+
 class CreateNoteAPI(CreateAPIView):
     # queryset = None
     serializer_class = NoteSaveSerializer
 
-# class NotesAPI(ListCreateAPIView):
-#     serializer_class=NoteSerializer
-#     queryset = Note.objects.all()
-
-#     def get_queryset(self):
-#         queryset=Note.objects.filter(author=self.request.user,cat = 1)
-#         return queryset
 
 class NotesAPI(ListCreateAPIView):
     serializer_class = NoteSerializer
@@ -50,6 +39,7 @@ class NotesAPI(ListCreateAPIView):
             queryset = Note.objects.filter(author=self.request.user, cat=cat)
         return queryset
     
+
 class NotesAPI22(ListCreateAPIView):
     serializer_class = NoteSerializer
     queryset = Note.objects.all()
@@ -86,12 +76,79 @@ class NoteRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
 
 
 
-def getNote(request):
-    pass
-def getListOfNotes(request):
-    pass
-def updateNote(request):
-    pass
+@api_view(["POST"])
+def createStageNote(request):
+    data=request.data.dict()
+    print(request.user)
+    print(data)
+    data["authorstage"]=request.user.id
+    serializer=StageNoteSaveSerializer(data=data)
+    if serializer.is_valid(raise_exception=True):
+        notestage=serializer.save()
+        return Response(StageNoteSerializer(notestage,many=False).data)
+    return Response({})
 
-def deleteNote(request):
-    pass
+
+
+
+
+class CreateStageNoteAPI(CreateAPIView):
+    # queryset = None
+    serializer_class = StageNoteSaveSerializer
+
+
+class StageNotesAPI(ListCreateAPIView):
+    serializer_class = StageNoteSerializer
+    queryset = StageNote.objects.all()
+    filter_backends = [SearchFilter]
+    search_fields = ["idnote"]
+
+    def get_queryset(self):
+        idnote = self.kwargs.get("idnote")
+        print(idnote)
+        if idnote is not None:
+            queryset = StageNote.objects.filter(authorstage=self.request.user, idnote=idnote)
+        return queryset
+
+
+class StageNotesAPI11(ListCreateAPIView):
+    serializer_class =StageNoteSerializer
+    queryset = StageNote.objects.all()
+    filter_backends = [SearchFilter]
+    search_fields = ["catstage"]
+
+    def get_queryset(self):
+        queryset = StageNote.objects.filter(catstage=1)
+        return queryset
+    
+
+
+    
+
+
+
+
+
+class StageNoteRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class =StageNoteSaveSerializer
+    queryset = StageNote.objects.all()
+    lookup_field = 'pk'
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer =StageNoteSerializer(instance)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        instance=serializer.save()
+        serializer = StageNoteSerializer (instance)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
