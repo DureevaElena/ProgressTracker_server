35d85af08e459387d57d655929333dbf1f60b0c5
DureevaElena <elenadureeva903@gmail.com>
2024-06-25 14:11:17 +0300

    docker

--- configurations/todo/admin.py
+++ configurations/todo/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import NoteTodo, StageNoteTodo
+from .models import NoteTodo, StageNoteTodo, CategoryTodo, StatusTodo, DoneTodo
 
 
 admin.site.register(NoteTodo)
 admin.site.register(StageNoteTodo)
+admin.site.register(CategoryTodo)
+admin.site.register(StatusTodo)
+admin.site.register(DoneTodo)
